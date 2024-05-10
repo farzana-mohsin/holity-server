1ee9a15a5ce4f0a47b2687ae8a43f5afca4fb094
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-09 21:24:06 -0400

    add GET api to fetch single data for posts related data

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ async function run() {
       res.send(result);
     });
 
+    // get a single post data
+    app.get("/post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await postsCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(

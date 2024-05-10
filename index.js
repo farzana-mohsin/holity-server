d3493998e8ea1977d03febeba70608b9d8b91a9e
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-09 21:22:07 -0400

    add database and two collections for the project

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const postsCollection = client.db("volunteers").collection("posts");
+    const applicationsCollection = client
+      .db("volunteers")
+      .collection("applications");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(

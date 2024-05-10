61a9bb02816c99a77bfa475c69d11c27337ea78d
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-09 21:23:29 -0400

    add POST api and test it with postman

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/posts", async (req, res) => {
+      const newPosts = req.body;
+      const result = await postsCollection.insertOne(newPosts);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
